0e3a17c12837a88703403a479f5b3ac378c9d3b2
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-05-06 16:14:37 +0200

    feat: improve 04-copy-directory fn to rewrite destfolder when delete in srcfolder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,31 +3,42 @@ const path = require('path');
 
 const srcFolder = path.join(__dirname, 'files');
 const targetFolder = path.join(__dirname, 'files-copy');
-console.log(srcFolder)
-console.log(targetFolder)
 
 function copyDirectory(srcFolder, destFolder) {
   fs.mkdir(destFolder, { recursive: true }, (err) => {
     if (err) throw err;
-
-    fs.readdir(srcFolder, { withFileTypes: true }, (err, files) => {
+    
+    fs.readdir(destFolder, { withFileTypes: true }, (err, destFiles) => {
       if (err) throw err;
 
-      files.forEach(file => {
-        const srcPath = path.join(srcFolder, file.name);
+      destFiles.forEach(file => {
         const destPath = path.join(destFolder, file.name);
 
-        if(file.isDirectory()) {
-          copyDirectory(srcPath, destPath);
-        } else {
-          const readStream = fs.createReadStream(srcPath);
-          const writeStream = fs.createWriteStream(destPath);
-          readStream.pipe(writeStream);
+        if (file.isFile()) {
+          fs.unlink(destPath, (err) => {
+            if(err) throw err;
+          });
         }
+      });
+
+      fs.readdir(srcFolder, { withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+  
+        files.forEach(file => {
+          const srcPath = path.join(srcFolder, file.name);
+          const destPath = path.join(destFolder, file.name);
+
+          if(file.isDirectory()) {
+            copyDirectory(srcPath, destPath);
+          } else {
+            const readStream = fs.createReadStream(srcPath);
+            const writeStream = fs.createWriteStream(destPath);
+            readStream.pipe(writeStream);
+          }
+        });
       });
     });
   });
 }
-
 
 copyDirectory(srcFolder, targetFolder);
