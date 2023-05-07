66402cf10e3507677beda29082d21899bc56f1f6
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-05-07 09:08:20 +0200

    feat: add function that copy assets folder in dist folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -12,11 +12,51 @@ const distFolder = path.join(__dirname, 'project-dist');
 fs.mkdir(distFolder, { recursive: true }, (err) => {
   if (err) throw err;
 });
+
 fs.mkdir(assetsDistFolder, { recursive: true }, (err) => {
   if (err) throw err;
 });
-fs.writeFile(stylesDist, (err) => {
+
+fs.writeFile(stylesDist, '', (err) => {
   if (err) throw err;
 });
 
+function copyDirectory(srcFolder, destFolder) {
+  fs.mkdir(destFolder, { recursive: true }, (err) => {
+    if (err) throw err;
+    
+    fs.readdir(destFolder, { withFileTypes: true }, (err, destFiles) => {
+      if (err) throw err;
+
+      destFiles.forEach(file => {
+        const destPath = path.join(destFolder, file.name);
+
+        if (file.isFile()) {
+          fs.unlink(destPath, (err) => {
+            if(err) throw err;
+          });
+        }
+      });
+
+      fs.readdir(srcFolder, { withFileTypes: true }, (err, files) => {
+        if (err) throw err;
+  
+        files.forEach(file => {
+          const srcPath = path.join(srcFolder, file.name);
+          const destPath = path.join(destFolder, file.name);
+
+          if(file.isDirectory()) {
+            copyDirectory(srcPath, destPath);
+          } else {
+            const readStream = fs.createReadStream(srcPath);
+            const writeStream = fs.createWriteStream(destPath);
+            readStream.pipe(writeStream);
+          }
+        });
+      });
+    });
+  });
+}
+
+copyDirectory(assetsSrcFolder, assetsDistFolder);
 
